a098be367c0d9c485bb745eb06dd23c83e270a09
DinhSonHai <sdinhhai@gmail.com>
2020-08-29 10:57:42 +0700

    Init to do app react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/TodoItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.todoItems = [
+      'Đi xem phim', 
+      'Đi đá bóng', 
+      'Đi chợ'
+    ];
+}
+  render() {
+    return (
+      <div className="App">
+            {
+              this.todoItems.map((item, index) => <TodoItem key={ index } title={ item } />)
+            }
+          <img src="https://picsum.photos/200/300" alt=""></img>
+      </div>
+    );
+  }
 }
 
 export default App;
